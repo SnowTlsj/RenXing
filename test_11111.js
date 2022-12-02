f4073106f40d0786d5fcba48e4fcd498cc0e4754
拖拉斯基~ <94552711+SnowTlsj@users.noreply.github.com>
2022-12-03 01:03:27 +0800

    更新

--- test_11111.js
+++ test_11111.js
@@ -1,5 +1,7 @@
 /*
 个人test
 cron 0 0 * * * test_11111.js
-*/
 const $ = new Env('conc---111');
+*/
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://192.168.0.200:8080";
--- test_22222.js
+++ test_22222.js
@@ -1,5 +1,7 @@
 /*
 个人test
 cron 0 0 * * * test_22222.js
-*/
 const $ = new Env('conc---222');
+*/
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://192.168.0.200:8080";
